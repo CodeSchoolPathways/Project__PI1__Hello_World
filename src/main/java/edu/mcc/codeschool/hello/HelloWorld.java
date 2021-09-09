080c6e5a3c2f519f8c25be1f967e6fa942c38965
Evan Stohlmann <evans@gen6ventures.com>
2021-09-08 22:31:24 -0500

    Updating README.md

--- src/main/java/edu/mcc/codeschool/hello/HelloWorld.java
+++ src/main/java/edu/mcc/codeschool/hello/HelloWorld.java
@@ -1,7 +1,7 @@
 package edu.mcc.codeschool.hello;
 
 public class HelloWorld {
-  public static void main(String args[]){
+  public static void main(String... args) {
     System.out.println("Hello world");
   }
 }
